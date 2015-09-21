e648565aeeeb430ffa172fc09b057390a66d4c14
Olaf Luijks <oluijks@gmail.com>
2015-09-21 21:56:18 +0200

    Session error view update

--- app/Modules/Views/layout/partials/session-error.blade.php
+++ app/Modules/Views/layout/partials/session-error.blade.php
@@ -11,15 +11,15 @@
     <div class="alert alert-danger">
         <div class="row">
             <div class="col-md-3">
-                <img class="img-responsive hidden-xs"
-                    style="float: right;" src="{!! URL::asset('img/icon_error.png') !!}"
-                     alt=""
+                <img class="img-responsive hidden-xs session-error-icon"
+                     src="{!! URL::asset('img/icon_error.png') !!}"
+                     alt="{!! trans('validation.form_problems') !!}"
                      width="128"
                      height="128">
                 </p>
             </div>
             <div class="col-md-9">
-                <h4>{{ trans('validation.form_problems') }}</h4><br>
+                <h4>{!! trans('validation.form_problems') !!}</h4><br>
                 <ul class="list-unstyled">
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
